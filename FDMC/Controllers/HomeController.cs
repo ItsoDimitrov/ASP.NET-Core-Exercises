17377934ea19ef734c50549df1986b6796bee023
ItsoDimitrov <fffsssccc@abv.bg>
2018-11-10 15:40:33 +0200

    Implemented index page

--- FDMC/Controllers/HomeController.cs
+++ FDMC/Controllers/HomeController.cs
@@ -5,14 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FDMC.Models;
+using FDMC.Data;
+using FDMC.ViewModel;
 
 namespace FDMC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var cat = this._context.Cats.ToList();
+            var cats = new CatViewModel
+            {
+                Cats = cat
+            };
+            
+            return View(cats);
         }
 
      
